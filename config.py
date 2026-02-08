7ab87e6e884caeab86e6b98e8f8cbfab5c05aab9
ajchavez94 <andreajchavez94@gmail.com>
2026-02-08 16:34:38 +0100

    mas errores corregidos: water content, sla, etc.

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 #names sistematized
+#isotopes
 named13C_cellulose = "δ13C (‰ v.s.V-PDB)"
 named13C_bulk = "bulk_δ13C (‰ v.s.V-PDB)"
 name18O = "δ18O in ‰ vs DeltaSMOW"
 named15N = "δ15N (‰ v.s.AIR)"
-nameC_bulk ="[C_b]"
-nameC_cellulose = "[C]"
+iWUE = "iWUE (μmol/mol)"
+
+nameC_bulk ="[C_b]" #rename to Cleaf
+nameC_cellulose = "[C]" #rename to Ccellulse
+# functional traits
 thick = "thickness (mm)"
 mthough = "mean though"
 though = "force to punch (kN/m)"
@@ -15,3 +19,9 @@ leaf_water = "leaf water content"
 dry_matter = "dry mater content (mg/g)"
 leaf_dry = "Leaf dry weight (g)"
 leaf_fresh = "Leaf fresh weight (g)"
+
+### for nutrients ##
+Carea = "C_area" #Nleaf_C/SLA
+Narea = "N_area" #leaf_N/SLA
+
+functional_traits = [thick,mthough,LA,SLA,leaf_water,dry_matter,Carea,Narea]
